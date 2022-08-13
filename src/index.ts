5935f46bbb65ca5986a732c5fe1fb2d5f0234ccd
Per Mortensen <per@permortensen.com>
2022-08-13 10:49:12 +0200

    Require user interaction to flip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 import { v4 as uuidv4 } from "uuid";
+import { CoinFlip } from "./CoinFlip";
 import { errorHandler, notFoundHandler } from "./errors";
 import { createCoinFlip, createDatabase, randomElement } from "./helpers";
 import { HttpError } from "./HttpError";
@@ -13,45 +15,61 @@ app.set("trust proxy", true);
 app.set("view engine", "pug");
 app.use(express.urlencoded());
 
-app.get("/", (req, res) =>
-  res.status(200).render("index", { options: ["heads", "tails"] })
-);
-
-app.post("/flip", async (req, res, next) => {
+app.param("uuid", async (req, _res, next, uuid: string) => {
   try {
-    const options = getOptions(req.body);
-    const coinFlip = createCoinFlip(uuidv4(), options, req.ip);
-    await db.createCoinFlip(coinFlip);
-
-    res.status(201).render("link", { link: generateLink(req, coinFlip.uuid) });
-  } catch (err) {
-    next(err);
-  }
-});
-
-app.get("/flip/:uuid", async (req, res, next) => {
-  try {
-    const requestIp = req.ip;
-    const uuid = req.params.uuid;
-
     let coinFlip = await db.getCoinFlip(uuid);
 
     if (!coinFlip) {
       throw new HttpError(404, "Coin flip not found");
     }
 
-    if (!coinFlip.result) {
-      coinFlip.result = randomElement(coinFlip.options);
-      coinFlip.flippedAt = new Date();
-      coinFlip.flippedIp = requestIp;
-      await db.updateCoinFlip(coinFlip);
-    }
-
-    res.status(200).render("flip", { coinFlip, requestIp });
+    (req as any).coinFlip = coinFlip;
+    next();
   } catch (err) {
     next(err);
   }
 });
+
+app.get("/", (_req, res) =>
+  res.status(200).render("index", { options: ["heads", "tails"] })
+);
+
+app.post(
+  "/flip",
+  asyncHandler(async (req, res) => {
+    const options = getOptions(req.body);
+    const coinFlip = createCoinFlip(uuidv4(), options, req.ip);
+    await db.createCoinFlip(coinFlip);
+
+    res.status(201).render("link", { link: generateLink(req, coinFlip.uuid) });
+  })
+);
+
+app
+  .route("/flip/:uuid")
+  .get(
+    asyncHandler(async (req, res) => {
+      const requestIp = req.ip;
+      const coinFlip = (req as any).coinFlip as CoinFlip;
+
+      res.status(200).render("flip", { coinFlip, requestIp });
+    })
+  )
+  .post(
+    asyncHandler(async (req, res) => {
+      const requestIp = req.ip;
+      const coinFlip = (req as any).coinFlip as CoinFlip;
+
+      if (!coinFlip.result) {
+        coinFlip.result = randomElement(coinFlip.options);
+        coinFlip.flippedAt = new Date();
+        coinFlip.flippedIp = requestIp;
+        await db.updateCoinFlip(coinFlip);
+      }
+
+      res.status(200).render("flip", { coinFlip, requestIp });
+    })
+  );
 
 app.use(notFoundHandler);
 app.use(errorHandler);
