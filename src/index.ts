ba0ed42d7a855b18e60dca83d8d275addeac0eda
Per Mortensen <per@permortensen.com>
2025-07-09 21:56:06 +0200

    Upgrade express to v5

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 import { v4 as uuidv4 } from "uuid";
 import { CoinFlip } from "./CoinFlip";
 import { errorHandler, notFoundHandler } from "./errors";
@@ -20,61 +19,50 @@ if (process.env.TRUST_PROXY === "true") {
 app.use(express.urlencoded({ extended: true }));
 
 app.param("uuid", async (req, _res, next, uuid: string) => {
-  try {
-    let coinFlip = await db.getCoinFlip(uuid);
+  let coinFlip = await db.getCoinFlip(uuid);
 
-    if (!coinFlip) {
-      throw new HttpError(404, "Coin flip not found");
-    }
-
-    (req as any).coinFlip = coinFlip;
-    next();
-  } catch (err) {
-    next(err);
+  if (!coinFlip) {
+    throw new HttpError(404, "Coin flip not found");
   }
+
+  (req as any).coinFlip = coinFlip;
+  next();
 });
 
 app.get("/", (_req, res) =>
   res.status(200).render("index", { options: ["heads", "tails"] })
 );
 
-app.post(
-  "/flip",
-  asyncHandler(async (req, res) => {
-    const options = getOptions(req.body);
-    const requestIp = getRequestIp(req);
-    const coinFlip = createCoinFlip(uuidv4(), options, requestIp);
-    await db.createCoinFlip(coinFlip);
+app.post("/flip", async (req, res) => {
+  const options = getOptions(req.body);
+  const requestIp = getRequestIp(req);
+  const coinFlip = createCoinFlip(uuidv4(), options, requestIp);
+  await db.createCoinFlip(coinFlip);
 
-    res.status(201).render("link", { link: generateLink(req, coinFlip.uuid) });
-  })
-);
+  res.status(201).render("link", { link: generateLink(req, coinFlip.uuid) });
+});
 
 app
   .route("/flip/:uuid")
-  .get(
-    asyncHandler(async (req, res) => {
-      const requestIp = getRequestIp(req);
-      const coinFlip = (req as any).coinFlip as CoinFlip;
+  .get(async (req, res) => {
+    const requestIp = getRequestIp(req);
+    const coinFlip = (req as any).coinFlip as CoinFlip;
 
-      res.status(200).render("flip", { coinFlip, requestIp });
-    })
-  )
-  .post(
-    asyncHandler(async (req, res) => {
-      const requestIp = getRequestIp(req);
-      const coinFlip = (req as any).coinFlip as CoinFlip;
+    res.status(200).render("flip", { coinFlip, requestIp });
+  })
+  .post(async (req, res) => {
+    const requestIp = getRequestIp(req);
+    const coinFlip = (req as any).coinFlip as CoinFlip;
 
-      if (!coinFlip.result) {
-        coinFlip.result = randomElement(coinFlip.options);
-        coinFlip.flippedAt = new Date();
-        coinFlip.flippedIp = requestIp;
-        await db.updateCoinFlip(coinFlip);
-      }
+    if (!coinFlip.result) {
+      coinFlip.result = randomElement(coinFlip.options);
+      coinFlip.flippedAt = new Date();
+      coinFlip.flippedIp = requestIp;
+      await db.updateCoinFlip(coinFlip);
+    }
 
-      res.status(200).render("flip", { coinFlip, requestIp });
-    })
-  );
+    res.status(200).render("flip", { coinFlip, requestIp });
+  });
 
 app.use(notFoundHandler);
 app.use(errorHandler);
